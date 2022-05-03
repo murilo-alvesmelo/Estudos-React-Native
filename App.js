f84abca252106241c1fed4650400b8f3255f6039
Murilo Alves <murilo.melo72@hotmail.com>
2022-05-03 10:53:34 -0300

    Iniciando mini projeto - Gerador Mega sena

--- App.js
+++ App.js
@@ -1,84 +1,10 @@
 import React from "react"
 import {View, StyleSheet, SafeAreaView} from 'react-native'
-
-//import FlexBoxV4 from "./src/components/layout/FlexBoxV4"
-//import FlexBoxV3 from "./src/components/layout/FlexBoxV3"
-//import FlexBoxV2 from "./src/components/layout/FlexBoxV2"
-//import FlexBoxV1 from "./src/components/layout/FlexBoxV1"
-//import Quadrado from "./src/components/layout/Quadrado"
-//import DigiteSeuNome from "./src/components/DigiteSeuNome"
-//import Listaprodutos from "./src/components/produtos/Listaprodutos"
-//import UsuarioLogado from "./src/components/UsuarioLogado"
-//import Membro from "./src/components/relacao/Membro"
-//import ContadorV2 from "./src/components/contador/ContadorV2"
-//import ParImpar from "./src/components/indereta/ParImpar"
-//import Pai from "./src/components/indereta/Pai"
-//import Aleatorio from "./src/components/Aleatorio";
-//import Botao from "./src/components/Botao";
-//import Minmax from "./src/components/Minmax";
-//import Contador from "./src/components/Contador";
-//import CompPadrao, { Comp1, Comp2 } from "./src/components/Multi";
-//import Primeiro from "./src/components/Primeiro"
-//import Soma from "./src/components/Soma";
-//import Titulo from "./src/components/Titulo";
-
-/* function App(){
-    return(
-        <View>
-            <Primeiro/>
-        </View>
-    );
-}
-
-export default App */
-
-/* const App = function() {
-    return(
-        <View>
-            <Primeiro/>
-        </View>
-    );
-}
-export default App */
-
-/* export default function(){
-    return(
-        <View>
-            <Primeiro/>
-        </View>
-    );
-} */
+import { Mega } from "./src/components/mega/Mega"
 
 export default () => (
         <SafeAreaView style={style.App}>
-            {/*
-            <FlexBoxV4/>
-            <FlexBoxV3/>
-            <FlexBoxV2/>
-            <FlexBoxV1/>
-            <DigiteSeuNome/>
-            <Listaprodutos />
-            <UsuarioLogado usuario={{ nome: 'Murilo', email: 'murilo@icloud.com'}}/>
-            <Membro/>
-            <ParImpar num={2}/>
-            <ContadorV2/>
-            <Pai/>
-            <Contador/>
-            <Botao/>
-            <Titulo principal='Previpalmas' secundario="Bem vindo"/>
-            <Aleatorio min={1} max={60}/>
-            <Aleatorio min={1} max={60}/>
-            <Aleatorio min={1} max={60}/>
-            <Aleatorio min={1} max={60}/>
-            <Aleatorio min={1} max={60}/>
-            <Aleatorio min={1} max={60}/>
-            <Minmax min={3} max={90}/>
-            <Minmax min={5} max={60}/>
-            <Soma num1={10} num2={10}/>
-            <Primeiro/>
-            <CompPadrao/>
-            <Comp1/>
-            <Comp2/> */}
+            <Mega qtdNumeros={6}/>
         </SafeAreaView>
 )
 
